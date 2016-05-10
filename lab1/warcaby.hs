a334cb76909f0149c16304b925080e40f04e676f
micherpl <browaryxyz@gmail.com>
2016-05-11 01:10:53 +0200

    Update warcaby.hs

--- lab1/warcaby.hs
+++ lab1/warcaby.hs
@@ -1,1 +1,44 @@
+module Warcaby where
 
+import System.IO
+
+
+type Board = [[Square]]
+data Piece = Piece PColor PType 
+data PColor = White | Black 
+data PType = Pawn | Queen 
+
+initialBoardStr = unlines [" b b b b"
+                          ,"b b b b "
+                          ," b b b b"
+                          ,"        "
+                          ,"        "
+                          ,"w w w w "
+                          ," w w w w"
+                          ,"w w w w "
+						]
+
+showBoard :: Board -> String
+showBoard = unlines . map showRow
+  where showRow = map showSquare
+showSquare :: Square -> Char
+showSquare = maybe ' ' showPiece
+
+readBoard :: String -> Board
+readBoard = map readRow . lines
+  where readRow = map readSquare
+readSquare :: Char -> Square
+readSquare ' ' = Nothing
+readSquare c   = Just (readPiece c)
+
+showPiece :: Piece -> Char
+showPiece (Piece White Pawn)   = 'w'
+showPiece (Piece White Queen)  = 'W'
+showPiece (Piece Black Pawn)   = 'b'
+showPiece (Piece Black Queen)  = 'B'
+
+readPiece :: Char -> Piece
+readPiece 'w' = (Piece White Pawn)
+readPiece 'W' = (Piece White Queen)
+readPiece 'b' = (Piece Black Pawn)
+readPiece 'B' = (Piece Black Queen)
